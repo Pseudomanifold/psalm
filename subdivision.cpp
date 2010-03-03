9e660116ee064339993d01aac3f829602f59a6ab
Bastian Rieck <bastian.rieck@annwfn.net>
2010-03-03 21:46:58 +0100

    Working draft for main program
    User-specified options are read and interpreted properly now. Program is
    almost usable.

--- subdivision.cpp
+++ subdivision.cpp
@@ -4,8 +4,11 @@
 */
 
 #include <iostream>
+#include <sstream>
 
 #include <GL/glut.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "subdivision.h"
 #include "mesh.h"
@@ -15,6 +18,8 @@ using namespace std;
 
 mesh scene_mesh;
 string input;
+string output;
+
 bool draw_lines = true;
 
 void display_scene();
@@ -24,16 +29,121 @@ void keyboard_callback(unsigned char, int, int);
 
 int main(int argc, char* argv[])
 {
-	// quick and dirty
-	if(argc == 1)
+	static option cmd_line_opts[] =
 	{
-		cout << "Loading default mesh...\n";
-		input = "";
+		{"output",	required_argument,	NULL,	'o'},
+		{"steps",	required_argument,	NULL,	'n'},
+		{"type",	required_argument,	NULL,	't'},
+
+		{"help",	no_argument,		NULL,	'h'},
+
+		{NULL, 0, NULL, 0}
+	};
+
+	short type	= mesh::TYPE_EXT;
+	size_t steps	= 1;
+
+	int option = 0;
+	while((option = getopt_long(argc, argv, "o:n:t:h", cmd_line_opts, NULL)) != -1)
+	{
+		switch(option)
+		{
+			case 'o':
+				output = optarg;
+				break;
+
+			case 't':
+			{
+				string type_str = optarg;
+				transform(type_str.begin(), type_str.end(), type_str.begin(), (int(*)(int)) tolower);
+
+				if(type_str == "ply")
+					type = mesh::TYPE_PLY;
+				else if(type_str == "obj")
+					type = mesh::TYPE_OBJ;
+				else if(type_str == "off")
+					type = mesh::TYPE_OFF;
+				else
+					cerr << "WTF? UNKNOWN TYPE.\n"; // FIXME
+
+				break;
+			}
+
+			case 'n':
+			{
+				istringstream converter(optarg);
+				converter >> steps;
+				if(converter.fail())
+				{
+					// FIXME
+					cerr << "CONVERTER ERROR\n";
+				}
+				break;
+			}
+
+			case 'h':
+			case '?':
+				// NYI
+				break;
+		}
 	}
-	else
+
+	// Read further command-line parameters; these are all supposed to be
+	// input files. If the user already specified an output file, only one
+	// input file will be accepted.
+
+	vector<string> files;
+	while(optind < argc)
 	{
-		cout << "Load user-specified mesh \"" << argv[1] << "\"\n";
-		input = argv[1];
+		files.push_back(argv[optind++]);
+		if(output.length() != 0 && files.size() > 1)
+		{
+			cerr << "WTF?\n"; // FIXME
+			return(0);
+		}
+	}
+
+	// Replace the special file "-" by an empty string, thereby signalling
+	// that standard input and standard output are to be used as file
+	// streams.
+
+	for(vector<string>::iterator it = files.begin(); it != files.end(); it++)
+	{
+		if(it->length() == 1 && (*it)[0] == '-')
+			*it = "";
+	}
+
+	bool output_set = (output.length() > 0);
+	if(output.length() == 1 && output[0] == '-')
+		output = "";
+
+	// Apply subdivision algorithm to all files
+
+	for(vector<string>::iterator it = files.begin(); it != files.end(); it++)
+	{
+		scene_mesh.load(*it, type);
+
+		// FIXME: Let algorithm be chosen
+		for(size_t i = 0; i < steps; i++)
+			scene_mesh.subdivide_catmull_clark();
+
+		// If an output file has been set (even if it is empty), it
+		// will be used.
+		if(output_set)
+			scene_mesh.save(output, type);
+
+		// If no output file has been set and the input file name is
+		// not empty, the output will be written to a file.
+		else if(it->length() > 0)
+		{
+			// FIXME: File name can be determined automatically
+			scene_mesh.save(*it+".subdivided", type);
+		}
+
+		// If no output file has been set and the input file name is
+		// empty, the output will be written to STDOUT.
+		else
+			scene_mesh.save("", type);
 	}
 
 	glutInit(&argc, argv);
