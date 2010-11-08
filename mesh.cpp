7f38e19674a4f89c9ac279b95c922dacac4c409a
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2010-11-08 16:36:19 +0100

    Fixed bug in file extension handling
    Need to handle errors better. Furthermore, same fix is required for the
    save_*() functions.

--- mesh.cpp
+++ mesh.cpp
@@ -128,7 +128,7 @@ bool mesh::load(const std::string& filename, short type)
 	// Filename given, data type identification by extension
 	if(filename.length() >= 4 && type == TYPE_EXT)
 	{
-		std::string extension = filename.substr(filename.length()-4);
+		std::string extension = filename.substr(filename.find_last_of('.')); // TODO: Handle errors
 		std::transform(extension.begin(), extension.end(), extension.begin(), (int(*)(int)) tolower);
 
 		if(extension == ".ply")
