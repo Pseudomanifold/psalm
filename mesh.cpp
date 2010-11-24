15ee93a02ecce41dfc4c229f76d6649cf7516fd3
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2010-11-24 15:26:54 +0100

    Reverting heuristical algorithms [I]
    This reverts commit e61ed3730ef420fbf47021887d88b3e092008557. The
    algorithm has been removed because further work will be done in an
    auxiliary program.

--- mesh.cpp
+++ mesh.cpp
@@ -930,7 +930,6 @@ bool mesh::load_pline(std::istream& in)
 		std::vector<vertex*> pline;
 
 		converter >> id_label >> num_vertices;
-
 		for(size_t i = 0; i < num_vertices; i++)	// ignore last point because it is a repetition of
 								// the first point
 		{
@@ -940,16 +939,10 @@ bool mesh::load_pline(std::istream& in)
 			pline.push_back(add_vertex(x,y,z));
 		}
 
-#ifdef SECOND_ALGO
-
 		// Second version of algorithm: Create vertices for boundary and pick points
 		// at random.
 		pick_points();
-
 		break;
-#endif
-
-#ifdef FIRST_ALGO
 
 		// First version of algorithm: Compute geometric centre of the hole and place an
 		// extraordinary vertex of sufficient valency there.
@@ -1019,82 +1012,10 @@ bool mesh::load_pline(std::istream& in)
 			previous_midpoint = midpoint2;
 		}
 
-#endif
-
-#define THIRD_ALGO
-#ifdef THIRD_ALGO
-		// Third version of algorithm: Compute geometric centre of the
-		// hole and place an extraordinary vertex of sufficient valency
-		// there.
-		v3ctor centre;
-		for(size_t i = 0; i < pline.size(); i++)
-			centre += pline[i]->get_position()*(1.0/pline.size());
-
-		vertex* centre_vertex = add_vertex(centre);
-
-		// For each two adjacent vertices of the hole, create a
-		// midpoint and connect the two vertices and the midpoint with
-		// the centre vertex in order to form a simple quadrangulation
-		for(size_t i = 0; i < pline.size(); i++)
-		{
-			size_t next;
-			if(i == pline.size()-1)
-				next = 0;
-			else
-				next = i+1;
-
-			vertex* midpoint = add_vertex(	pline[i]->get_position()*0.5+
-							pline[next]->get_position()*0.5);
-
-			midpoint->boundary = true;
-			pline[i]->boundary = true;
-			pline[next]->boundary = true;
-
-			add_face(pline[i], midpoint, pline[next], centre_vertex);
-		}
-#endif
-
-#ifdef FOURTH_ALGO
-		// Fourth version of algorithm: Compute geometric centre of the
-		// hole and place an extraordinary vertex of sufficient valency
-		// there.
-		v3ctor centre;
-		for(size_t i = 0; i < pline.size(); i++)
-			centre += pline[i]->get_position()*(1.0/pline.size());
-
-		vertex* centre_vertex = add_vertex(centre);
-
-		// Scale original vertices and translate them in direction of
-		// the hole. Create quadrangular faces by connecting
-		//
-		//	vertex 1 -- copy 1 -- copy 2 -- vertex 2
-		//
-		// and adding an n-sided face afterwards.
-
-		std::vector<vertex*> pline_copy;
-		for(size_t i = 0; i < pline.size(); i++)
-			pline_copy.push_back(add_vertex(pline[i]->get_position()*0.25+centre*0.75));
-
-		for(size_t i = 0; i < pline.size(); i++)
-		{
-			size_t next;
-			if(i == pline.size()-1)
-				next = 0;
-			else
-				next = i+1;
-
-			pline[i]->boundary = true;
-			pline[next]->boundary = true;
-
-			add_face(pline_copy[i], pline_copy[next], pline[next], pline[i]);
-			add_face(pline_copy[next], pline_copy[i], centre_vertex);
-		}
-#endif
-
-		break;
-
 		converter.clear();
 		line.clear();
+
+		break;
 	}
 
 	return(true);
@@ -2430,7 +2351,6 @@ void mesh::cc_create_points_p(mesh& M,
 		else
 		{
 			std::pair<double, double> weights = weight_function(n);
-			//std::pair<double, double> weights = std::make_pair(0.7, 0.1); // <dev>
 
 			gamma	= weights.second;
 			beta	= weights.first;
