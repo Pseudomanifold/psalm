328e0bf023624fb5a6c7e22909706c445a7758c2
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2010-12-10 18:23:58 +0100

    Added preliminary version of libpsalm

---
+++ libpsalm.h
@@ -0,0 +1,13 @@
+/*!
+*	@file	libpsalm.h
+*	@brief	Exports a function for filling holes. Used by GigaMesh.
+*	@author Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
+*/
+
+#ifndef __LIBPSALM_H__
+#define __LIBPSALM_H__
+
+void fill_hole(	int num_vertices, long* vertex_IDs, double* coordinates,
+		int* num_new_vertices, double** new_coordinates, int* num_new_faces, long** new_vertex_IDs);
+
+#endif
