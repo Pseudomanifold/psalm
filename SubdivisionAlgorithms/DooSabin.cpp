2bc2c71035d961ab782db08c8d0221a74ec44add
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2010-12-17 15:58:41 +0100

    Corrected `DooSabin` algorithm

--- SubdivisionAlgorithms/DooSabin.cpp
+++ SubdivisionAlgorithms/DooSabin.cpp
@@ -9,6 +9,15 @@ namespace psalm
 {
 
 /*!
+*	Sets default attributes for the Doo-Sabin algorithm.
+*/
+
+DooSabin::DooSabin()
+{
+	use_geometric_point_creation = true;
+}
+
+/*!
 *	Applies Doo and Sabin's subdivision algorithm to the given mesh. The
 *	mesh will be irreversibly _changed_ by this function.
 *
@@ -20,8 +29,181 @@ bool DooSabin::apply_to(mesh& input_mesh)
 {
 	mesh output_mesh;
 
+	if(use_geometric_point_creation)
+		create_face_vertices_geometrically(input_mesh, output_mesh);
+	else
+		create_face_vertices_parametrically(input_mesh, output_mesh);
+
+	create_f_faces(input_mesh, output_mesh);
+	create_e_faces(input_mesh, output_mesh);
+	create_v_faces(input_mesh, output_mesh);
+
+	input_mesh.replace_with(output_mesh);
+	return(true);
+}
+
+/*!
+*	Creates the new face vertices for the Doo-Sabin scheme. This algorithm
+*	follows the geometrical approach as presented in the paper of Doo and
+*	Sabin.
+*
+*	@param input_mesh	Original input mesh, will not be modified
+*	@param output_mesh	Mesh that will contain the new face vertices
+*/
+
+void DooSabin::create_face_vertices_geometrically(mesh& input_mesh, mesh& output_mesh)
+{
+	for(size_t i = 0; i < input_mesh.num_faces(); i++)
+	{
+		print_progress(	"Creating points [geometrically]",
+				i,
+				input_mesh.num_faces()-1);
+
+		face* f = input_mesh.get_face(i);
+
+		// Find centroid of face
+		v3ctor centroid;
+		for(size_t j = 0; j < f->num_vertices(); j++)
+		{
+			const vertex* v = f->get_vertex(j);
+			centroid += v->get_position();
+		}
+		centroid *= 1.0/f->num_vertices();
+
+		// For a fixed vertex of the face, find the two edges that are
+		// incident on this vertex and calculate their midpoints.
+		for(size_t j = 0; j < f->num_vertices(); j++)
+		{
+			const vertex* v = f->get_vertex(j);
+
+			const edge* e1 = NULL;
+			const edge* e2 = NULL;
+			for(size_t k = 0; k < f->num_edges(); k++)
+			{
+				if(	f->get_edge(k).e->get_u() == v ||
+					f->get_edge(k).e->get_v() == v)
+				{
+					if(e1 == NULL)
+						e1 = f->get_edge(k).e;
+					else
+					{
+						e2 = f->get_edge(k).e;
+						break;
+					}
+				}
+			}
+
+			// TODO: Handle error by returning false
+			if(e1 == NULL || e2 == NULL)
+				throw(std::runtime_error("DooSabin::create_face_vertices_parametrically(): One of the stored edges is NULL -- unable to continue"));
+
+			// Calculate midpoints of the edges and the position of
+			// face vertex
+
+			v3ctor midpoint1;
+			v3ctor midpoint2;
+
+			midpoint1 = (e1->get_u()->get_position()+e1->get_v()->get_position())/2;
+			midpoint2 = (e2->get_u()->get_position()+e2->get_v()->get_position())/2;
+
+			v3ctor face_vertex_position = (midpoint1+midpoint2+centroid+v->get_position())/4;
+
+			// Add new vertex to new mesh and store it in the
+			// vector of face vertices for the old face -- this
+			// vector will be used when creating the topology of
+			// the new mesh.
+
+			vertex* face_vertex = output_mesh.add_vertex(face_vertex_position);
+			f->add_face_vertex(face_vertex);
+		}
+	}
+}
+
+/*!
+*	Creates the new face vertices for the Doo-Sabin scheme. This algorithm
+*	employs a parametrical approach, thereby making it possible for the
+*	user to specify different weights in order to fine-tune the algorithm.
+*
+*	@param input_mesh	Original input mesh, will not be modified
+*	@param output_mesh	Mesh that will contain the new face vertices
+*/
+
+void DooSabin::create_face_vertices_parametrically(mesh& input_mesh, mesh& output_mesh)
+{
+	// Only used if extra_weights has been defined
+	weights_map::const_iterator it;
+	std::vector<double> weights;
+
+	for(size_t i = 0; i < input_mesh.num_faces(); i++)
+	{
+		print_progress(	"Creating points [parametrically]",
+				i,
+				input_mesh.num_faces()-1);
+
+		face* f = input_mesh.get_face(i);
+
+		size_t n = f->num_vertices();
+		std::vector<const vertex*> vertices = sort_vertices(f, f->get_vertex(0));
+
+		// Check if weights for a face with n vertices can be found
+		weights.clear();
+		if(	custom_weights.size() != 0 &&
+			((it = custom_weights.find(n)) != custom_weights.end()))
+			weights = it->second;
+
+		for(size_t j = 0; j < vertices.size(); j++)
+		{
+			v3ctor face_vertex_position;
+
+			// If user-defined weights are present and weights for the current
+			// number of vertices have been found
+			if(!weights.empty())
+			{
+				for(size_t k = 0; k < weights.size(); k++)
+					face_vertex_position += vertices[k]->get_position()*weights[k];
+			}
+
+			// Use weight distribution function
+			else
+			{
+				// By default, use original weights for quadrangles
+				if(n == 4 && use_bspline_weights)
+				{
+					face_vertex_position =  vertices[0]->get_position()*9.0/16.0+
+								vertices[1]->get_position()*3.0/16.0+
+								vertices[2]->get_position()*1.0/16.0+
+								vertices[3]->get_position()*3.0/16.0;
+				}
+
+				else
+				{
+					for(size_t k = 0; k < vertices.size(); k++)
+						face_vertex_position += vertices[k]->get_position()*weight_function(n,k);
+				}
+			}
+
+			vertex* face_vertex = output_mesh.add_vertex(face_vertex_position);
+			f->add_face_vertex(face_vertex);
+
+			// Shift the vector
+			const vertex* v = vertices[0];
+			vertices.erase(vertices.begin());
+			vertices.push_back(v);
+		}
+	}
+}
+
+/*!
+*	Creates F-faces for the Doo-Sabin algorithm.
+*
+*	@param input_mesh	Original input mesh, will not be modified
+*	@param output_mesh	Mesh that will contain the new face vertices
+*/
+
+void DooSabin::create_f_faces(mesh& input_mesh, mesh& output_mesh)
+{
 	// Create new F-faces by connecting the appropriate vertex points
-	// (generated above) of the face
+	// (generated elsewhere) of the face
 	for(size_t i = 0; i < input_mesh.num_faces(); i++)
 	{
 		print_progress(	"Creating F-faces",
@@ -39,8 +221,17 @@ bool DooSabin::apply_to(mesh& input_mesh)
 
 		output_mesh.add_face(vertices);
 	}
+}
 
-	// Create quadrilateral E-faces
+/*!
+*	Creates quadrilateral E-faces for the Doo-Sabin algorithm.
+*
+*	@param input_mesh	Original input mesh, will not be modified
+*	@param output_mesh	Mesh that will contain the new face vertices
+*/
+
+void DooSabin::create_e_faces(mesh& input_mesh, mesh& output_mesh)
+{
 	for(size_t i = 0 ; i < input_mesh.num_edges(); i++)
 	{
 		print_progress(	"Creating E-faces",
@@ -79,7 +270,17 @@ bool DooSabin::apply_to(mesh& input_mesh)
 
 		output_mesh.add_face(v1, v2, v3, v4);
 	}
+}
 
+/*!
+*	Creates V-faces for the Doo-Sabin algorithm.
+*
+*	@param input_mesh	Original input mesh, will not be modified
+*	@param output_mesh	Mesh that will contain the new face vertices
+*/
+
+void DooSabin::create_v_faces(mesh& input_mesh, mesh& output_mesh)
+{
 	// Create V-faces by connecting the face vertices of all faces that are
 	// adjacent to a fixed vertex.
 	for(size_t i = 0; i < input_mesh.num_vertices(); i++)
@@ -108,8 +309,79 @@ bool DooSabin::apply_to(mesh& input_mesh)
 
 		output_mesh.add_face(vertices);
 	}
+}
 
-	return(true);
+/*!
+*	Given a face and a vertex v, sort all vertices of the face in
+*	counterclockwise order, starting with vertex v.
+*
+*	@param f Pointer to the face
+*	@param v Pointer to the "first" vertex
+*
+*	@return Sorted vector of vertices
+*/
+
+std::vector<const vertex*> DooSabin::sort_vertices(face* f, const vertex* v)
+{
+	std::vector<const vertex*> vertices;
+
+	size_t pos_v = std::numeric_limits<std::size_t>::max();
+	directed_edge d_edge_v;
+
+	for(size_t i = 0; i < f->num_edges(); i++)
+	{
+		d_edge_v = f->get_edge(i);
+		if(	d_edge_v.e->get_u() == v ||
+			d_edge_v.e->get_v() == v)
+		{
+			pos_v = i;
+			break;
+		}
+	}
+
+	if(pos_v == std::numeric_limits<size_t>::max())
+		throw(std::runtime_error("DooSabin::sort_vertices(): Unable to find vertex"));
+
+	bool take_first = false;	// signals whether the first or the second
+					// edge is to be taken for each edge
+
+	if(d_edge_v.e->get_u() == v)
+	{
+		vertices.push_back(d_edge_v.e->get_u());
+		take_first = !d_edge_v.inverted;
+	}
+	else
+	{
+		vertices.push_back(d_edge_v.e->get_v());
+		take_first = d_edge_v.inverted;
+	}
+
+	for(size_t i = pos_v; (i-pos_v) < f->num_edges(); i++)
+	{
+		const vertex* w;
+		directed_edge d_e;
+
+		// Index must wrap around once the end is reached
+		if(i >= f->num_edges())
+			d_e = f->get_edge(i-f->num_edges());
+		else
+			d_e = f->get_edge(i);
+
+		// Always take the _same_ vertex of each edge (depending on the
+		// position of v)
+		if(d_e.inverted)
+			w = (take_first ? d_e.e->get_v() : d_e.e->get_u());
+		else
+			w = (take_first ? d_e.e->get_u() : d_e.e->get_v());
+
+		// Avoid duplicates. They can only occur with v because v will
+		// always be the first vertex in the result vector regardless
+		// of whether it was the first or second vertex of the edge
+		if(w != v)
+			vertices.push_back(w);
+	}
+
+	return(vertices);
 }
 
 /*!
--- SubdivisionAlgorithms/DooSabin.h
+++ SubdivisionAlgorithms/DooSabin.h
@@ -7,25 +7,70 @@
 #define __DOO_SABIN_H__
 
 #include <vector>
+#include <map>
 
-#include "SubdivisionAlgorithm.h"
+#include "BsplineSubdivisionAlgorithm.h"
 
 namespace psalm
 {
+
+/*!
+*	Typedef for describing a map that contains the weights for a k-sided
+*	face.
+*/
+
+typedef std::map<size_t, std::vector<double> > weights_map;
 
 /*!
 *	@class DooSabin
 *	@brief Doo-Sabin subdivision algorithm
 */
 
-class DooSabin : public SubdivisionAlgorithm
+class DooSabin : public BsplineSubdivisionAlgorithm
 {
-	public:
+	 public:
+		DooSabin();
+
 		bool apply_to(mesh& input_mesh);
 
 	private:
+		void create_face_vertices_geometrically(mesh& input_mesh, mesh& output_mesh);
+		void create_face_vertices_parametrically(mesh& input_mesh, mesh& output_mesh);
+
+		void create_f_faces(mesh& input_mesh, mesh& output_mesh);
+		void create_e_faces(mesh& input_mesh, mesh& output_mesh);
+		void create_v_faces(mesh& input_mesh, mesh& output_mesh);
+
+		std::vector<const vertex*> sort_vertices(face* f, const vertex* v);
 		std::vector<face*> sort_faces(vertex* v);
 		vertex* find_face_vertex(face* f, const vertex* v);
+
+		/*!
+			This pointer will be set to an appropriate predefined
+			weight function for the Doo-Sabin scheme.
+
+			@see weights_doo_sabin()
+			@see weights_catmull_clark()
+			@see weights_degenerate()
+		*/
+
+		double (*weight_function)(size_t, size_t);
+
+		static double weights_doo_sabin(size_t k, size_t i);
+		static double weights_catmull_clark(size_t k, size_t i);
+		static double weights_degenerate(size_t k, size_t i);
+
+		/*!
+			Custom weights, i.e. weights that are only required for
+			certain valencies.
+		*/
+
+		weights_map custom_weights;
+
+		bool use_geometric_point_creation;	///< Flag signalling that new face vertices are
+							///< supposed to be created geometrically instead
+							///< of using parametrical (i.e. weight-based)
+							///< methods
 };
 
 } // end of namespace "psalm"
