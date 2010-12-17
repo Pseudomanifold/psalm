c4db630c9f44283fa7e41aaaee8998289d722612
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2010-12-17 19:04:55 +0100

    Modified test program to include Doo-Sabin scheme

--- sa_test.cpp
+++ sa_test.cpp
@@ -1,8 +1,12 @@
 #include "SubdivisionAlgorithms/Loop.h"
+#include "SubdivisionAlgorithms/DooSabin.h"
 
 int main(int argc, char* argv[])
 {
 	psalm::mesh M;
+
+	// LOOP
+
 	M.load("../Meshes/Icosahedron.ply");
 
 	psalm::Loop loop_scheme;
@@ -10,6 +14,18 @@ int main(int argc, char* argv[])
 	{
 		loop_scheme.apply_to(M);
 	}
-	M.save("../test.ply");
+	M.save("../test_loop.ply");
+
+	// DS
+
+	M.load("../Meshes/Icosahedron.ply");
+
+	psalm::DooSabin doo_sabin_scheme;
+	for(int i = 0; i < 6; i++)
+	{
+		doo_sabin_scheme.apply_to(M);
+	}
+	M.save("../test_doo_sabin.ply");
+
 	return(0);
 }
