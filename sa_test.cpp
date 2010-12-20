af56d3965f98ce960c5c7a99d69327c14e210e9e
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2010-12-20 14:27:57 +0100

    Added test for Catmull-Clark algorithm

--- sa_test.cpp
+++ sa_test.cpp
@@ -1,5 +1,6 @@
 #include "SubdivisionAlgorithms/Loop.h"
 #include "SubdivisionAlgorithms/DooSabin.h"
+#include "SubdivisionAlgorithms/CatmullClark.h"
 
 int main(int argc, char* argv[])
 {
@@ -26,6 +27,17 @@ int main(int argc, char* argv[])
 		doo_sabin_scheme.apply_to(M);
 	}
 	M.save("../test_doo_sabin.ply");
+
+	// CC
+
+	M.load("../Meshes/Icosahedron.ply");
+
+	psalm::CatmullClark catmull_clark_scheme;
+	for(int i = 0; i < 6; i++)
+	{
+		catmull_clark_scheme.apply_to(M);
+	}
+	M.save("../test_catmull_clark.ply");
 
 	return(0);
 }
