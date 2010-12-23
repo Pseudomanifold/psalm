8903a8fa9f1450aeccf5898ad78fbdc67a1ca895
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2010-12-23 14:34:51 +0100

    More tests in density test program
    Preliminary results of gnuplot fit:
    
    Final set of parameters            Asymptotic Standard Error
    =======================            ==========================
    
    a0              = 6.08864e-07      +/- 2.317e-08    (3.805%)
    a1              = -0.00167679      +/- 4.792e-05    (2.858%)
    b0              = -1.61397e-06     +/- 1.884e-08    (1.167%)
    b1              = 0.00500891       +/- 2.852e-05    (0.5694%)
    c0              = 1.77846          +/- 0.01856      (1.044%)
    
    f(x,y) = a0*x**2+a1*x+b0*y**2+b1*y+c0

--- density_test.cpp
+++ density_test.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
+#include <utility>
 
 #include "v3ctor.h"
 #include "SubdivisionAlgorithms/Liepa.h"
 
-double area_and_density(psalm::mesh& M)
+std::pair<double, double> area_and_density(psalm::mesh& M, double initial_area = 0.0)
 {
-	double area = 0.0;
-	for(size_t i = 0; i < M.num_faces(); i++)
-	{
-		psalm::face* f = M.get_face(i);
-		for(size_t j = 0; j < 3; j++)
-		{
-			v3ctor a = f->get_vertex(1)->get_position() - f->get_vertex(0)->get_position();
-			v3ctor b = f->get_vertex(2)->get_position() - f->get_vertex(0)->get_position();
+	std::pair<double, double> result;
 
-			area += 0.5*(a|b).length();
+	result.first = initial_area;
+	if(initial_area == 0.0)
+	{
+		for(size_t i = 0; i < M.num_faces(); i++)
+		{
+			psalm::face* f = M.get_face(i);
+			for(size_t j = 0; j < 3; j++)
+			{
+				v3ctor a = f->get_vertex(1)->get_position() - f->get_vertex(0)->get_position();
+				v3ctor b = f->get_vertex(2)->get_position() - f->get_vertex(0)->get_position();
+
+				result.first += 0.5*(a|b).length();
+			}
 		}
 	}
+	result.second = M.num_vertices()/result.first;
 
 	std::cerr	<< "(area, density) = " << "("
-			<< std::fixed << std::setprecision(4) << area << ","
-			<< std::fixed << std::setprecision(4) << (M.num_vertices()/area)
+			<< std::fixed << std::setprecision(4) << result.first << ","
+			<< std::fixed << std::setprecision(4) << result.second
 			<< ")" << "\n";
 
-	return(M.num_vertices()/area);
+	return(result);
 }
 
 int main(int argc, char* argv[])
@@ -33,14 +41,46 @@ int main(int argc, char* argv[])
 	psalm::mesh M;
 	psalm::Liepa liepa_scheme;
 
-	M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0008.ply");
-	std::cout << "INITIAL: " << area_and_density(M) << "\n";
+	if(argc == 1)
+		return(0);
 
+	for(int i = 2; i < argc; i++)
+	{
+		M.load(argv[i]);
+		std::pair<double, double> area_density = area_and_density(M);
+
+		double initial_area = area_density.first;
+		double initial_density = area_density.second;
+
+		for(int j = 0; j < 49; j++)
+		{
+			double alpha = j*0.1;
+
+			M.load(argv[i]);
+
+			liepa_scheme.set_alpha(alpha);
+			liepa_scheme.apply_to(M);
+
+			std::cout	<< (initial_density) << "\t"
+					<< (initial_area) << "\t"
+					<< (area_and_density(M, initial_area).second-initial_density) << "\t"
+					<< (alpha)
+					<< std::endl;
+		}
+
+		std::cout << "\n\n";
+	}
+
+	return(0);
 
 	/*
-	for(int i = 0; i <= 10; i++)
+
+	//M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0008.ply");
+	//std::cout << "INITIAL: " << area_and_density(M) << "\n";
+
+	for(int i = 1; i <= 100; i++)
 	{
-		double alpha = i*3;
+		double alpha = i*0.05;
 
 		M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0025.ply");
 
@@ -49,12 +89,66 @@ int main(int argc, char* argv[])
 
 		std::cout << (alpha) << "\t" << area_and_density(M) << std::endl;
 	}
+
+	//M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0008.ply");
+	//liepa_scheme.set_alpha(sqrt(2));
+	//liepa_scheme.apply_to(M);
+
+	std::cout << "\n\n";
+	for(int i = 1; i <= 100; i++)
+	{
+		double alpha = i*0.05;
+
+		M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0008.ply");
+
+		liepa_scheme.set_alpha(alpha);
+		liepa_scheme.apply_to(M);
+
+		std::cout << (alpha) << "\t" << area_and_density(M) << std::endl;
+	}
+
+	std::cout << "\n\n";
+	for(int i = 1; i <= 100; i++)
+	{
+		double alpha = i*0.05;
+
+		M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0009.ply");
+
+		liepa_scheme.set_alpha(alpha);
+		liepa_scheme.apply_to(M);
+
+		std::cout << (alpha) << "\t" << area_and_density(M) << std::endl;
+	}
+
+
+	std::cout << "\n\n";
+	for(int i = 1; i <= 100; i++)
+	{
+		double alpha = i*0.05;
+
+		M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0010.ply");
+
+		liepa_scheme.set_alpha(alpha);
+		liepa_scheme.apply_to(M);
+
+		std::cout << (alpha) << "\t" << area_and_density(M) << std::endl;
+	}
+
+	std::cout << "\n\n";
+	for(int i = 1; i <= 100; i++)
+	{
+		double alpha = i*0.05;
+
+		M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0011.ply");
+
+		liepa_scheme.set_alpha(alpha);
+		liepa_scheme.apply_to(M);
+
+		std::cout << (alpha) << "\t" << area_and_density(M) << std::endl;
+	}
+
 	*/
 
-	M.load("../Holes/Salmanassar3_Holes_w_Normals_HR_CLEAN_with_Indices.pline_0000_0008.ply");
-	liepa_scheme.set_alpha(sqrt(2));
-	liepa_scheme.apply_to(M);
-
-	std::cout << "END: " << area_and_density(M) << "\n";
+	//std::cerr << "END: " << area_and_density(M) << "\n";
 	return(0);
 }
