d087f2621ef96a5c761879c82d029b0097fcbf90
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2011-01-19 16:32:41 +0100

    Added triangulation algorithms
    * Abstract base class
    * Generic minimum weight triangulation algorithm

---
+++ TriangulationAlgorithms/TriangulationAlgorithm.cpp
@@ -0,0 +1,27 @@
+/*!
+*	@file	TriangulationAlgorithm.cpp
+*	@brief	Functions for general triangulation algorithm class
+*/
+
+#include "TriangulationAlgorithm.h"
+
+namespace psalm
+{
+
+/*!
+*	Empty constructor
+*/
+
+TriangulationAlgorithm::TriangulationAlgorithm()
+{
+}
+
+/*!
+*	Empty destructor
+*/
+
+TriangulationAlgorithm::~TriangulationAlgorithm()
+{
+}
+
+} // end of namespace "psalm"
