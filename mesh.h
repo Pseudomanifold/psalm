bc6d33adde5c091865e74b0eedb5c7872b1af42a
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2011-01-20 13:38:51 +0100

    Made mesh::load_raw_data() and mesh::save_raw_data() public

--- mesh.h
+++ mesh.h
@@ -80,6 +80,9 @@ class mesh
 		bool load(const std::string& filename, file_type type = TYPE_EXT);
 		bool save(const std::string& filename, file_type type = TYPE_EXT);
 
+		void load_raw_data(int num_vertices, long* vertex_IDs, double* coordinates);
+		void save_raw_data(int* num_new_vertices, double** new_coordinates, int* num_faces, long** vertex_IDs);
+
 		void prune(	const std::set<size_t>& remove_faces,
 				const std::set<size_t>& remove_vertices);
 		void subdivide(	subdivision_algorithm algorithm = mesh::ALG_CATMULL_CLARK,
@@ -181,9 +184,6 @@ class mesh
 		bool save_obj(std::ostream& out);
 		bool save_off(std::ostream& out);
 		bool save_hole(std::ostream& out);
-
-		void load_raw_data(int num_vertices, long* vertex_IDs, double* coordinates);
-		void save_raw_data(int* num_new_vertices, double** new_coordinates, int* num_faces, long** vertex_IDs);
 
 		bool use_parametric_point_creation;	//< Flag signalling that points in subdivision schemes shall
 							//< be computed using the parametric variant. Normally, the
