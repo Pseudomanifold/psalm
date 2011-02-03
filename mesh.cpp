5049d2a706bb674bb12ebb5ab2571a4323f0112b
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2011-02-03 14:22:17 +0100

    Rewrite: Removed `print_progress()` from `mesh` class

--- mesh.cpp
+++ mesh.cpp
@@ -1370,41 +1370,6 @@ void mesh::prune(const std::set<size_t>& remove_faces, const std::set<size_t>& r
 }
 
 /*!
-*	Prints a progress bar to STDOUT.
-*
-*	@param	op	Operation the progress bar shall show; will be expanded by ": "
-*	@param	cur_pos	Current position of progress bar
-*	@param	max_pos	Maximum position of progress bar
-*/
-
-void mesh::print_progress(std::string message, size_t cur_pos, size_t max_pos)
-{
-	if(!print_statistics)
-		return;
-
-	size_t percentage = (cur_pos*100)/max_pos;
-	static size_t last;
-
-	if(percentage - last < 5 && cur_pos != max_pos)
-		return;
-
-	std::cerr	<< "\r" << std::left << std::setw(50) << message << ": "
-			<< "[";
-
-
-	std::cerr	<< std::setw(10)
-			<< std::string( percentage/10, '#')
-			<< "]"
-			<< " "
-			<< std::setw(3) << percentage << "%" << std::right;
-
-	if(cur_pos == max_pos)
-		std::cerr << std::endl;
-
-	last = percentage;
-}
-
-/*!
 *	Applies a subdivision algorithm to the current mesh.
 *
 *	@param algorithm	Defines which algorithm to use (Catmull-Clark
