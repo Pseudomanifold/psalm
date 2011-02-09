92ff415a8a73e60d824d82053d05fb9ae7c01385
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2011-02-09 16:14:48 +0100

    libpsalm: Removed parameter estimation code

--- libpsalm.h
+++ libpsalm.h
@@ -10,11 +10,4 @@
 bool fill_hole(	int num_vertices, long* vertex_IDs, double* coordinates, double* scale_attributes, double* normals,
 		int* num_new_vertices, double** new_coordinates, int* num_new_faces, long** new_vertex_IDs);
 
-namespace libpsalm
-{
-
-double estimate_density(double input_density, double desired_density);
-
-} // end of namespace "libpsalm"
-
 #endif
